d2a8534a504d92f4ee524b96389454db4361cdc9
Fyodoor <91294516+Fyodoor@users.noreply.github.com>
2022-10-29 22:13:49 +0300

    Task_05

---
+++ lesson_02/Task_5.java
@@ -0,0 +1,16 @@
+package lesson_02.homework_02;
+
+public class Task_5 {
+    public static void main(String[] args) {
+        // Задача 5. В методе main расставь правильно знаки плюс и минус,
+        // чтобы значение переменной result получилось равным 20.
+        int a = 1;
+        int b = 3;
+        int c = 9;
+        int d = 27;
+
+        int result = -a + b - c + d;
+
+        System.out.println(result);
+    }
+}
