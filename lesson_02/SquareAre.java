fa54bf320945a636845790bf5441fa0b8ff66f8a
Fyodoor <91294516+Fyodoor@users.noreply.github.com>
2022-10-29 22:11:45 +0300

    Task_02

---
+++ lesson_02/SquareAre.java
@@ -0,0 +1,15 @@
+package lesson_02.homework_02;
+
+public class SquareAre {
+    public static void main(String[] args) {
+        // Задача 2. Дано: Стороны квадрата a = 50, b = 20. Найти площадь.
+
+        int a = 50;
+        int b = 20;
+
+        int are = a * b;
+
+        System.out.println("Площадь равна " + are); //Это не квадрат, а прямоугольник
+
+    }
+}
