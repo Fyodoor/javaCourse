b9b221896fa71678d8431a1ba4b10cc23af5bfcc
Fyodoor <91294516+Fyodoor@users.noreply.github.com>
2022-11-07 02:34:32 +0200

    Add files via upload

---
+++ lesson_04/Cycles/Task_02.java
@@ -0,0 +1,14 @@
+package lesson_03.homework;
+
+import java.util.Scanner;
+
+public class Task_02 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < i + 1; j++) {
+                System.out.print(8);
+            }
+            System.out.println();
+        }
+    }
+}
