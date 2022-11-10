96dd5ef089d040bcff9f6d15d0b75413d990bc87
Fyodoor <91294516+Fyodoor@users.noreply.github.com>
2022-11-10 23:34:26 +0200

    Update Task_06.java

--- lesson_04/Arrays/Task_06.java
+++ lesson_04/Arrays/Task_06.java
@@ -5,19 +5,19 @@ import java.util.Scanner;
 public class Task_06 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int [] numbers = new int[5];
+        int [] numbers = new int[20];
         int max = numbers[0];
         int min = numbers[0] + 1;
 
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < 20; i++) {
             int num = scanner.nextInt();
             numbers [i] = num;
         }
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < 20; i++) {
             if (numbers[i] > max) max = numbers[i];
         }
 
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < 20; i++) {
             if (numbers[i] < min) min = numbers[i];
         }
         System.out.println(max);
